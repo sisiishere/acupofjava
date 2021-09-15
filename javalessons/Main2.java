58011bfe52ec0c51335fd82d443475b71e3ffbf4
sisiishereb <set38246@marymount.edu>
2021-09-15 19:41:29 -0400

    editing the files

---
+++ javalessons/Main2.java
@@ -0,0 +1,9 @@
+package com.acupofjava; 
+import java.util.Date;
+public class Main2 {
+    public static void main(String[] args) {
+        byte age = 27;
+        Date now = new Date();
+        System.out.println("now");
+    }
+}
