8c26fadad2fe7ee8df6cd6801a48c4d16f2fb7b4
sisiishereb <set38246@marymount.edu>
2021-09-15 19:23:06 -0400

    adding the file and editing the files

--- javalessons/Main.java
+++ javalessons/Main.java
@@ -1,6 +1,8 @@
+package com.acupofjava; 
 public class Main {
     public static void main(String[] args) {
-        int age=27;
-        System.out.println(age);
+        int age = 27;
+        int herAge = age;
+        System.out.println(herAge);
     }
 }
---
+++ javalessons/Main1.java
@@ -0,0 +1,10 @@
+package com.acupofjava; 
+public class Main1 {
+    public static void main(String[] args) {
+        byte age = 27;
+        long viewsCount = 1_234_567_89L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isRelevant = true;
+    }
+}
