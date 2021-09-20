b9925cdd3f47674294ec162fc746d37fd08dde88
sisiishereb <set38246@marymount.edu>
2021-09-19 23:56:33 -0400

    editing the main file

--- javalessons/Main4.java
+++ javalessons/Main4.java
@@ -3,6 +3,6 @@ package com.javalessons;
 public class Main4 {
     public static void main(String[] args) {
         String message = "Hello, hello, hello!" + "This week, we will be playing with puppets!";
-        System.out.println(message);
+        System.out.println(message.length());
     }
 }
