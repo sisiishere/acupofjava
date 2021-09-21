e9347d4ee49a898e7767bd3c1e9e6778d81e590c
sisiishereb <set38246@marymount.edu>
2021-09-20 22:44:34 -0400

    editing the file

---
+++ javalessons/Main5.java
@@ -0,0 +1,8 @@
+package com.javalessons;
+
+public class Main5 {
+    public static void main(String[] args) {
+        String message = "Hello \"hello!\"" + "This week, we will be playing with puppets!";
+        System.out.println(message);
+    }
+}
