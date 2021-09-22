993cbe48b5deb9fbdcd5e5599a7245ab307ca178
sisiishereb <set38246@marymount.edu>
2021-09-22 02:48:30 -0400

    editing the file

---
+++ javalessons/Main7.java
@@ -0,0 +1,11 @@
+package com.javalessons;
+import java.util.Arrays;
+
+public class Main7{
+    public static void main(String[] args) {
+    int[] [] numbers = new int[2][3];
+    numbers[0][0] = 1;
+    //Arrays.sort(numbers);
+   System.out.println(Arrays.deepToString(numbers));
+    }
+}
