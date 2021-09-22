e215dae1f1073b38585f7454d7f16df7ac104aa7
sisiishereb <set38246@marymount.edu>
2021-09-22 03:17:09 -0400

    editing some changes to the files

--- javalessons/Main8.java
+++ javalessons/Main8.java
@@ -1,9 +1,8 @@
 package com.javalessons;
 import java.util.Arrays;
 
-public class Main7{
+public class Main8{
     public static void main(String[] args) {
-    int[] [] numbers = {{1, 2, 3, 4}, {5, 6, 7, 8}};
-   System.out.println(Arrays.deepToString(numbers));
+    final float PI = 3.14F;
     }
 }
