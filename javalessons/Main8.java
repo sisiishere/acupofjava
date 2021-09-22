a387dce95ddc12d76f751baee1f18be3aac90aad
sisiishereb <set38246@marymount.edu>
2021-09-22 03:06:58 -0400

    editing the file yet again

--- javalessons/Main8.java
+++ javalessons/Main8.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 public class Main7{
     public static void main(String[] args) {
     int[] [] numbers = {{1, 2, 3, 4}, {5, 6, 7, 8}};
-    numbers[0][0] = 1;
-    //Arrays.sort(numbers);
    System.out.println(Arrays.deepToString(numbers));
     }
 }
