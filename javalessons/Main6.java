6f1bc2f407defcdab4198f515c397b9d564d45f4
sisiishereb <set38246@marymount.edu>
2021-09-22 02:31:53 -0400

    edinting Main6.java

---
+++ javalessons/Main6.java
@@ -0,0 +1,10 @@
+package com.javalessons;
+import java.util.Arrays;
+
+public class Main6{
+    public static void main(String[] args) {
+    int[] numbers = {2, 4, 6, 8, 10};
+    Arrays.sort(numbers);
+   System.out.println(Arrays.toString(numbers));
+    }
+}
