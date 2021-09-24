db9c48747f4d75dc53070910eb695ee9db9c0e03
sisiishereb <set38246@marymount.edu>
2021-09-24 01:28:14 -0400

    editing the files

--- javalessons/Main12.java
+++ javalessons/Main12.java
@@ -1,18 +1,9 @@
 package com.javalessons;
 
 
-public class Main11{
+public class Main12{
 public static void main(String[] args) {
-    // Implicit casting
-    // byte > short > int > long > float > double
-    short x =1;
-    int y = x + 2;
-    double m = 1.1;
-    double n = m + 2;
-    String b = "1";
-    int c = Integer.parseInt(b) + 2; 
-    System.out.println(y);
-    System.out.println(n);
-    System.out.println(c);
+    int result = (int)Math.floor(1.1F);
+    System.out.println(result);
     }
 }
---
+++ javalessons/Main13.java
@@ -0,0 +1,9 @@
+package com.javalessons;
+
+
+public class Main12{
+public static void main(String[] args) {
+    int result = (int)Math.floor(1.1F);
+    System.out.println(result);
+    }
+}
