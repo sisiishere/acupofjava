f120bc3418edf8cc6c40f95d82e6886957f16daa
sisiishereb <set38246@marymount.edu>
2021-09-24 00:42:14 -0400

    editing the file

---
+++ javalessons/Main9.java
@@ -0,0 +1,11 @@
+package com.javalessons;
+import java.util.Arrays;
+
+public class Main9{
+    public static void main(String[] args) {
+    int x = 1;
+    int y = x++;
+    System.out.println(x);
+    System.out.println(y);
+    }
+}
