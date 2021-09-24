d26e3c6b3a68953b33f4732eda1bf41519351af8
sisiishereb <set38246@marymount.edu>
2021-09-24 02:07:41 -0400

    editing the files

---
+++ javalessons/Main14.java
@@ -0,0 +1,10 @@
+package com.javalessons;
+import java.util.Scanner;
+
+public class Main14{
+public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in);
+   byte age = scanner.nextByte();
+   System.out.println("You are " + age);
+}
+}
