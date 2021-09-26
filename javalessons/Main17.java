b6c293c7c889ce0d7d817f5e2a939397fa520ffb
sisiishereb <set38246@marymount.edu>
2021-09-26 13:32:20 -0400

    editing the file

---
+++ javalessons/Main17.java
@@ -0,0 +1,15 @@
+package com.javalessons;
+
+public class Main17 {
+    public static void main(String[] args) {
+        int temp = 32;
+        if (temp > 30) {
+            System.out.println("It is a hot day. Please, drink a lot of water.");
+        } else if(temp <20) {
+            System.out.println("It's a cold day. Please, layer up.");
+        } else {
+            System.out.println("It's a beautiful day. Please, enjoy it.")
+        }
+
+    }
+}
