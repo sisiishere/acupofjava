faf2867f9ae2019c4dea09bce692fd30215a43a3
sisiishereb <set38246@marymount.edu>
2021-09-26 13:23:28 -0400

    editing the file

--- javalessons/Main16.java
+++ javalessons/Main16.java
@@ -3,9 +3,10 @@ package com.javalessons;
 public class Main16 {
     public static void main(String[] args) {
         int temperature = 22;
-        boolean isMildAndBreezy = temperature > 20 && temperature < 30;
-        boolean isCold = temperature <= 20;
-        boolean isHot = temperature >= 30;
-        System.out.println(isMildAndBreezy);
+        boolean hasHighIncome = true;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolan isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+        System.out.println();
     }
 }
