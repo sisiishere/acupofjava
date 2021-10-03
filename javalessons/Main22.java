9ef0628036d27ffa9db04a720b93c64acc668d4d
sisiishereb <set38246@marymount.edu>
2021-10-03 00:52:23 -0400

    adding the file

---
+++ javalessons/Main22.java
@@ -0,0 +1,13 @@
+package com.codewithmosh;
+
+public class Main22 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = ""
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            
+            input = scanner.next();
+        }
+    }
+}
