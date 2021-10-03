0179dfd19a6ff8104a32d901e57aa4231da36ce7
sisiishereb <set38246@marymount.edu>
2021-10-03 01:58:09 -0400

    updating the files

--- javalessons/Main24.java
+++ javalessons/Main24.java
@@ -5,7 +5,7 @@ public class Main24 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = "";
-        while (!input.equals("quit")) {
+        while (!input.equals(" quit")) {
             System.out.print("Input: ");
             input = scanner.next().toLowerCase();
             if (input.equals("pass"))
