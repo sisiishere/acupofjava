077c57c4127fa250436924e39c59b65d52a956a4
sisiishereb <set38246@marymount.edu>
2021-10-03 01:11:42 -0400

    editing the files

--- javalessons/Main22.java
+++ javalessons/Main22.java
@@ -1,9 +1,10 @@
 package com.javalessons;
+import java.util.Scanner;
 
 public class Main22 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = ""
+        String input = "";
         while (!input.equals("quit")) {
             System.out.print("Input: ");
             
--- javalessons/Main23.java
+++ javalessons/Main23.java
@@ -1,13 +1,13 @@
 package com.javalessons;
+import java.util.Scanner;
 
 public class Main23 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = ""
-        while (!input.equals("quit")) {
+        String input = "";
+        do {
             System.out.print("Input: ");
-            
             input = scanner.next();
-        }
+        } while (!input.equals("quit"));
     }
 }
